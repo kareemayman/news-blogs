8fc49caede751681a8beeb3ed93e97f6a95c37d2
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-30 06:13:18 +0200

    Added Initial Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./layouts/Header"
+import Main from "./layouts/Main"
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <Main></Main>
     </div>
   )
 }
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <main>
+        
+        <div className="flex">
+            <div></div>
+            <div></div>
+        </div>
+
+        <div className="flex">
+            <div></div>
+            <div></div>
+        </div>
+
+        <div></div>
+
+        <div className="flex">
+            <div></div>
+            <div></div>
+        </div>
+
+    </main>
+  )
+}
