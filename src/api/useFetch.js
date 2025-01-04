f0a60bad07ff047ce6c2c4741b673807b965f37a
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-04 23:38:08 +0200

    Fetching News Intro Cards From GNews API

--- src/api/useFetch.js
+++ src/api/useFetch.js
@@ -1,35 +1,36 @@
-const GNewsAPIKey = import.meta.env.VITE_GNEWS_API_KEY
-const GNewsEndpoint = 'https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=eg&max=7&apikey=' + GNewsAPIKey
-
 import { useEffect, useState } from "react"
 
 export function useFetch(url) {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
-    useEffect(() => {
-        fetch(url).then((res) => {
-            if (!res.ok) {
-                throw Error('Could not fetch the data for that resource')
-            }
-            return res.json()
-        }).then((data) => {
-            () => {setData(data)}
-            () => {setError(false)}
-        }).catch((err) => {
-            () => {setError(true)}
-            console.log(err.message)
-        }).finally(() => {
-            () => {setLoading(false)}
-        })
-
-        return () => {
-            () => {setData(null)}
-            () => {setError(false)}
-            () => {setLoading(true)}
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource")
         }
-    }, [url])
+        return res.json()
+      })
+      .then((data) => {
+        setData(data)
+        setError(false)
+      })
+      .catch((err) => {
+        setError(true)
+        console.log(err.message)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
 
-    return { data, loading, error }
+    return () => {
+      setData(null)
+      setError(false)
+      setLoading(true)
+    }
+  }, [url])
+
+  return { data, loading, error }
 }
--- src/layouts/News.jsx
+++ src/layouts/News.jsx
@@ -5,23 +5,74 @@ import blog2 from "/src/assets/images/blog2.jpg"
 import health from "/src/assets/images/health.jpg"
 import tech from "/src/assets/images/tech.jpg"
 import science from "/src/assets/images/science.jpg"
-import world from "/src/assets/images/world.jpg" 
+import world from "/src/assets/images/world.jpg"
 import nation from "/src/assets/images/nation.jpg"
+import { useFetch } from "../api/useFetch"
+
+const GNewsAPIKey = import.meta.env.VITE_GNEWS_API_KEY
+let GNewsEndpoint =
+  "https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=7&apikey=" +
+  GNewsAPIKey
 
 export default function News() {
+  const { data, loading, error } = useFetch(GNewsEndpoint)
+  console.log(data)
+
   return (
     <>
       <div className="news-highlight">
-        <Card img={blog2} bookmark={true} title="children know unhealthy foods become abcs"></Card>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error Fetching News From API...</div>
+        ) : (
+          <Card
+            img={data.articles[0].image}
+            bookmark={true}
+            title={data.articles[0].title}
+          ></Card>
+        )}
       </div>
 
       <div className="news-grid">
-        <Card img={health} bookmark={true} title="children know unhealthy foods become abcs"></Card>
-        <Card img={tech} bookmark={true} title="children know unhealthy foods become abcs"></Card>
-        <Card img={science} bookmark={true} title="children know unhealthy foods become abcs"></Card>
-        <Card img={world} bookmark={true} title="children know unhealthy foods become abcs"></Card>
-        <Card img={nation} bookmark={true} title="children know unhealthy foods become abcs"></Card>
-        <Card img={tech} bookmark={true} title="children know unhealthy foods become abcs"></Card>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error Fetching News From API...</div>
+        ) : (
+          <>
+            <Card
+              img={data.articles[1].image}
+              bookmark={true}
+              title={data.articles[1].title}
+            ></Card>
+            <Card
+              img={data.articles[2].image}
+              bookmark={true}
+              title={data.articles[2].title}
+            ></Card>
+            <Card
+              img={data.articles[3].image}
+              bookmark={true}
+              title={data.articles[3].title}
+            ></Card>
+            <Card
+              img={data.articles[4].image}
+              bookmark={true}
+              title={data.articles[4].title}
+            ></Card>
+            <Card
+              img={data.articles[5].image}
+              bookmark={true}
+              title={data.articles[5].title}
+            ></Card>
+            <Card
+              img={data.articles[6].image}
+              bookmark={true}
+              title={data.articles[6].title}
+            ></Card>
+          </>
+        )}
       </div>
     </>
   )
