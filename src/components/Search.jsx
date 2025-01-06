17e56ea5852f751d5b5e3009a491ec18f26a9be3
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-06 20:58:57 +0200

    Added Delayed Search Functionality Without Submitting

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,30 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { NewsContext } from "../context/NewsContext"
 
 const GNewsAPIKey = import.meta.env.VITE_GNEWS_API_KEY
 
 function Search({ placeholder }) {
   const [search, setSearch] = useState("")
+  const [queryLastingFor2Secs, setQueryLastingFor2Secs] = useState("")
   const { newsEndpoint, setNewsEndpoint } = useContext(NewsContext)
+
+  // Search For News If User Stopped Typing For 2+ Seconds
+  useEffect(() => {
+    const timeoutHandler = setTimeout(() => {
+      setQueryLastingFor2Secs(search)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeoutHandler)
+    }
+  }, [search])
+  useEffect(() => {
+    if (queryLastingFor2Secs === search && search.trim() !== "") {
+      setNewsEndpoint(
+        `https://gnews.io/api/v4/search?q="${search}"&lang=en&country=us&max=7&apikey=${GNewsAPIKey}`
+      )
+    }
+  }, [queryLastingFor2Secs])
 
   function handleSubmit(event) {
     event.preventDefault()
