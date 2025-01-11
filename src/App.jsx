b2ce17eb658c2ea37f0e2d3f9e8058b1c9ccfd18
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-11 15:14:24 +0200

    Created Footer On Desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { NewsProvider } from "./context/NewsContext"
 import Header from "./layouts/Header"
 import Main from "./layouts/Main"
+import Footer from "./layouts/Footer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header></Header>
         <Main></Main>
       </NewsProvider>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <h3>NEWS & BLOGS APP</h3>
+        <p>&copy; Coded & Developed By Kareem Ayman</p>
+    </footer>
+  )
+}
