8fd575154302abd1030da711665b1a6e2613ac69
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-12 10:03:12 +0200

    Installed Date-fns Library, Formatted Date and Finshed NewsModal jsx

--- src/components/NewsModal.jsx
+++ src/components/NewsModal.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react"
+import { format } from "date-fns"
 
 export default function NewsModal({ data, img, title }) {
+    
+  const formattedDate = format(
+    new Date(data.publishedAt),
+    "MMM dd, yyyy, hh:mm a"
+  )
+
   return (
     <div className="news-modal-box">
-        <div className="img">
-            <img src={img} alt="article image" />
-        </div>
+      <div className="img">
+        <img src={img} alt="article image" />
+      </div>
 
-        <h3>{title}</h3>
+      <h3>{title}</h3>
 
-        <p className="source">Source: <a href={data.source.url}>{data.source.name}</a></p>
+      <p className="source">
+        Source: <a href={data.source.url}>{data.source.name}</a>
+      </p>
 
-        <p className='date'></p>
+      <p className="date">{formattedDate}</p>
+
+      <p className="description">{data.description}</p>
+
+      <div className="read-more">
+        <a href={data.url}>READ MORE</a>
+      </div>
     </div>
   )
 }
