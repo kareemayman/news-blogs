6750f9862fa694303390a594a098cb540bbdf00d
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-24 08:12:49 +0200

    Finished Search State, Form Submit Functionality In Weather Component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,22 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Weather() {
+  const [weatherSearch, setWeatherSearch] = useState("")
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    setWeatherSearch("")
+  }
+
   return (
     <div className="weather">
-        <h2 className="comfortaa">
-            <i className="fa-solid fa-location-dot"></i> Tbilisi
-        </h2>
+      <h2 className="comfortaa">
+        <i className="fa-solid fa-location-dot"></i> Tbilisi
+      </h2>
 
-        <form>
-            <input type="text" name="weather-search" id="weather-search" placeholder="Enter Location"/>
-            <button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
-        </form>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
+        <input
+          type="text"
+          name="weather-search"
+          id="weather-search"
+          placeholder="Enter Location"
+          value={weatherSearch}
+          onChange={(e) => setWeatherSearch(e.target.value)}
+        />
+        <button type="submit">
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
 
-        <div className="weather__info">
-            <h3>Sunny</h3>
-            <h3 className="comfortaa">Clear</h3>
-            <h3 className="comfortaa">30&deg;</h3>
-        </div>
+      <div className="weather__info">
+        <h3>Sunny</h3>
+        <h3 className="comfortaa">Clear</h3>
+        <h3 className="comfortaa">30&deg;</h3>
+      </div>
     </div>
   )
 }
