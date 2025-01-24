2c96017eae24e1b2a0109e482ac508f8b7811780
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-24 12:32:01 +0200

    Finished Weather Component & Made It Functional

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from "react"
+import { useFetch } from "../hooks/useFetch"
+
+// OpenWeather API Endpoint: https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=metric
+const OpenWeatherAPIKey = import.meta.env.VITE_OPENWEATHER_API_KEY
 
 export default function Weather() {
   const [weatherSearch, setWeatherSearch] = useState("")
+  const [currentSelectedCountry, setCurrentSelectedCountry] = useState("Cairo")
+  const [openWeatherEndpoint, setOpenWeatherEndpoint] = useState(
+    `https://api.openweathermap.org/data/2.5/weather?q=${currentSelectedCountry}&appid=${OpenWeatherAPIKey}&units=metric`
+  )
+  const { data, loading, error } = useFetch(openWeatherEndpoint)
 
   function handleFormSubmit(e) {
     e.preventDefault()
+    if (weatherSearch.trim() != "") {
+      setCurrentSelectedCountry(weatherSearch)
+      setOpenWeatherEndpoint(
+        `https://api.openweathermap.org/data/2.5/weather?q=${weatherSearch}&appid=${OpenWeatherAPIKey}&units=metric`
+      )
+    }
     setWeatherSearch("")
   }
 
   return (
     <div className="weather">
       <h2 className="comfortaa">
-        <i className="fa-solid fa-location-dot"></i> Tbilisi
+        <i className="fa-solid fa-location-dot"></i> {currentSelectedCountry}
       </h2>
 
       <form onSubmit={(e) => handleFormSubmit(e)}>
@@ -29,9 +44,65 @@ export default function Weather() {
       </form>
 
       <div className="weather__info">
-        <i className='bx bxs-sun' ></i>
-        <h3 className="comfortaa">Clear</h3>
-        <h3 className="comfortaa">30&deg;</h3>
+        {loading ? (
+          "Fetching Weather Information From API..."
+        ) : error ? (
+          "Error Fetching Weather Information From API..."
+        ) : (
+          <>
+            {data.weather[0].main === "Clear" &&
+              data.dt >= data.sys.sunrise &&
+              data.dt < data.sys.sunset && <i className="bx bxs-sun"></i>}
+            {data.weather[0].main === "Clear" &&
+              (data.dt < data.sys.sunrise || data.dt >= data.sys.sunset) && (
+                <i className="bx bxs-moon"></i>
+              )}
+            {data.weather[0].main === "Clouds" && (
+              <i className="bx bxs-cloud"></i>
+            )}
+            {data.weather[0].main === "Rain" && (
+              <i className="bx bxs-cloud-rain"></i>
+            )}
+            {data.weather[0].main === "Drizzle" && (
+              <i className="bx bxs-cloud-drizzle"></i>
+            )}
+            {data.weather[0].main === "Thunderstorm" && (
+              <i className="bx bxs-cloud-lightning"></i>
+            )}
+            {data.weather[0].main === "Snow" && (
+              <i className="bx bxs-cloud-snow"></i>
+            )}
+            {data.weather[0].main === "Mist" && (
+              <i className="bx bxs-cloud-fog"></i>
+            )}
+            {data.weather[0].main === "Haze" && (
+              <i className="bx bxs-cloud-haze"></i>
+            )}
+            {data.weather[0].main === "Fog" && (
+              <i className="bx bxs-cloud-fog2"></i>
+            )}
+            {data.weather[0].main === "Smoke" && (
+              <i className="bx bxs-cloud-smoke"></i>
+            )}
+            {data.weather[0].main === "Dust" && (
+              <i className="bx bxs-cloud-dust"></i>
+            )}
+            {data.weather[0].main === "Sand" && (
+              <i className="bx bxs-cloud-sand"></i>
+            )}
+            {data.weather[0].main === "Ash" && (
+              <i className="bx bxs-cloud-hail"></i>
+            )}
+            {data.weather[0].main === "Squall" && (
+              <i className="bx bxs-cloud-wind"></i>
+            )}
+            {data.weather[0].main === "Tornado" && (
+              <i className="bx bxs-tornado"></i>
+            )}
+            <h3 className="comfortaa">{data.weather[0].main}</h3>
+            <h3 className="comfortaa">{Math.round(data.main.temp)}&deg;</h3>
+          </>
+        )}
       </div>
     </div>
   )
