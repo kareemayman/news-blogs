5e5677e3ea5f257ad85e4499000c7a0555c51904
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-29 11:27:20 +0200

    Finshed New Post Form Page JSX & Style, & Made It Responsive

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,20 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function CreateBlog({ CreateBlog, setCreateBlog }) {
+  const [showForm, setShowForm] = useState(false)
+
+  function showFormFunction(e) {
+    e.target.style.display = "none"
+    setShowForm(true)
+  }
+
   return (
     <div className="create-blog">
       <div className="wallpaper"></div>
 
       <div className="create-blog__form">
-        <button className="create-post-button">CREATE NEW POST</button>
-
         <h2 className="back-button" onClick={() => setCreateBlog(false)}>
           BACK <i className="fa-solid fa-angle-right"></i>
         </h2>
+
+        <button
+          className="create-post-button"
+          onClick={(e) => showFormFunction(e)}
+        >
+          CREATE NEW POST
+        </button>
+
+        {showForm && (
+          <form>
+            <h1 className="comfortaa">NEW POST</h1>
+
+            <label htmlFor="imageUpload">
+              <i className="fa-solid fa-arrow-up-from-bracket"></i>
+              Upload Image
+              <input type="file" id="imageUpload" accept="image/*" />
+            </label>
+
+            <input
+              className="comfortaa"
+              type="text"
+              placeholder="Add Title (Max 60 characters)"
+            />
+
+            <textarea className="comfortaa" placeholder="Add Text"></textarea>
+
+            <button type="submit" className="comfortaa">
+              SUBMIT POST
+            </button>
+          </form>
+        )}
       </div>
 
-      <div className="profile-picture">
-      </div>
+      <div className="profile-picture"></div>
     </div>
   )
 }
